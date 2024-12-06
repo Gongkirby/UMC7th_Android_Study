16e0a2c453b80746ace2e4bed95353bd5a7a8f90
Gongkirby <9040__@naver.com>
2024-12-06 18:51:03 +0900

    10주차 스터디

--- UMC_Week10/app/build.gradle.kts
+++ UMC_Week10/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -33,6 +35,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
@@ -47,4 +53,24 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.kakao.sdk:v2-user:2.20.6") // 카카오 로그인 API 모듈
+
+    // naver login
+    implementation(files("libs/oauth-5.10.0.aar"))
+
+    implementation("com.airbnb.android:lottie:3.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.legacy:legacy-support-core-utils:1.0.0")
+    implementation("androidx.browser:browser:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("androidx.core:core-ktx:1.3.0")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.2.1")
+
 }
--- UMC_Week10/app/src/main/java/com/example/umc_week10/MainActivity.kt
+++ UMC_Week10/app/src/main/java/com/example/umc_week10/MainActivity.kt
@@ -5,46 +5,73 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_week10.databinding.ActivityMainBinding
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import com.kakao.sdk.user.model.User
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.OAuthLoginCallback
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // 로그인 버튼 설정
         val loginButton: ImageButton = findViewById(R.id.loginBut)
         loginButton.setOnClickListener {
             kakaoLogin()
         }
+
+        NaverIdLoginSDK.initialize(
+            this,
+            getString(R.string.naver_client_id),
+            getString(R.string.naver_client_secret),
+            getString(R.string.naver_client_name)
+        )
+
+        binding.buttonOAuthLoginImg.setOAuthLogin(object : OAuthLoginCallback {
+            override fun onSuccess() {
+                Log.d("test", "AccessToken : " + NaverIdLoginSDK.getAccessToken())
+                Log.d("test", "client id : " + getString(R.string.naver_client_id))
+                Log.d("test", "ReFreshToken : " + NaverIdLoginSDK.getRefreshToken())
+                Log.d("test", "Expires : " + NaverIdLoginSDK.getExpiresAt().toString())
+                Log.d("test", "TokenType : " + NaverIdLoginSDK.getTokenType())
+                Log.d("test", "State : " + NaverIdLoginSDK.getState().toString())
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Log.e("test", "$errorCode $errorDescription")
+            }
+
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        })
     }
 
     private fun kakaoLogin() {
         if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
             // 카카오톡 로그인
             UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                // 로그인 실패 부분
                 if (error != null) {
                     Log.e("Fail", "로그인 실패", error)
-                    // 사용자가 취소
                     if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
                         return@loginWithKakaoTalk
-                    }
-                    // 다른 오류
-                    else {
+                    } else {
                         UserApiClient.instance.loginWithKakaoAccount(
                             this,
                             callback = mCallback
-                        ) // 카카오 이메일 로그인
+                        )
                     }
-                }
-                // 로그인 성공 부분
-                else if (token != null) {
+                } else if (token != null) {
                     Log.e("Success", "로그인 성공 ${token.accessToken}")
                     UserApiClient.instance.me { user, error ->
                         if (error != null) {
@@ -56,7 +83,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         } else {
-            UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
+            UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback)
         }
     }
 
@@ -68,3 +95,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
